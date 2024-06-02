2b5974b35f11ac42a3824b0ce787da977f10c5f4
Vadim Makarov <add4che@gmail.com>
2024-06-03 00:56:00 +0300

    update hello world application to use axum

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,39 +1,19 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use axum::{routing::get, Router};
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+async fn root() -> &'static str {
+    "Hello, World!"
 }
 
-#[get("/foo")]
-async fn foo() -> impl Responder {
-    HttpResponse::Ok().body("Foo")
-}
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(root));
 
-#[get("/bar")]
-async fn bar() -> impl Responder {
-    HttpResponse::Ok().body("Bar")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    let port = std::env::var("PORT").unwrap_or(String::from("8000")).parse::<u16>().unwrap();
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-    })
-    .bind(("0.0.0.0", port))?
-    .run()
-    .await
+    let port = std::env::var("PORT")
+        .unwrap_or(String::from("8000"))
+        .parse::<u16>()
+        .unwrap();
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", port))
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
